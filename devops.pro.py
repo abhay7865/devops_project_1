8f7e961386000d7c9307fcb1ca597945ea8a646f
abhay7865 <2023000910.abhay@ug,sharda.ac.in>
2025-07-07 01:32:03 +0530

    passcomment2

--- devops.pro.py
+++ devops.pro.py
@@ -1,1 +1,14 @@
-git remote set-url origin git@github.com:abhay7865/devops_project_1.git
+
+from flask import Flask
+
+app = Flask(_name_)
+
+@app.route('/info')
+def info():
+    return "Heyy!! This is the info page."
+
+@app.route('/phone') 
+def phone():
+    return "This is the phone page."
+
+app.run(host='0.0.0.0', debug=True)
